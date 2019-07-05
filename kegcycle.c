3b44f0bc1625f379c9331197168b443b423ea2d1
Leo Cornelius <kegcoingold@gmail.com>
2019-07-05 21:35:36 +0100

    begin raw part ofkegcycle and move to. c

--- kegcycle.c
+++ kegcycle.c
@@ -1,6 +1,8 @@
 #include <blake2.h>
+#include <aesb.c>
 #define DATASET_SIZE 2500000000 //in bytes
 #define HASH_BYTES 32 // Lenght of each hash
+
 
 for(uint32_t i=0; i<DATASET_SIZE/HASH_BYTES;i++){
     calcDatasetItem(seed, i, &dataset[i*HASH_BYTES]);
@@ -9,3 +11,12 @@ void calcDatasetItem(uint8_t* seed, uint32_t itemNumber, uint8_t* out){
     blake2s(out, 32, itemNumber, 4, seed, 32);
 }
 
+void rawCycle(uint64_t data, uint64_t output) {
+    uint32_t data_32 = (uint32_t*) data;
+    uint32_t data32Temp = data_32;
+    crc32i(data_32);
+    aes(data_32, data);
+    for (uint32_t i = 1; i < 10; i++) {
+        aes((uint8*)data_32, (uint8*)data_32, (uint8_t*)data);
+    }
+}
