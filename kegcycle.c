6a3f405af601d8d870f2f04f3cbb27c162a2a62b
Leo Cornelius <kegcoingold@gmail.com>
2019-07-05 21:42:06 +0100

    update dataset create

--- kegcycle.c
+++ kegcycle.c
@@ -3,10 +3,15 @@
 #define DATASET_SIZE 2500000000 //in bytes
 #define HASH_BYTES 32 // Lenght of each hash
 
-
-for(uint32_t i=0; i<DATASET_SIZE/HASH_BYTES;i++){
-    calcDatasetItem(seed, i, &dataset[i*HASH_BYTES]);
-}
+void createDataset(uint8_t seed, uint8_t out) {
+  out = (uint8_t*)malloc(DATASET_SIZE);
+  if (!out) {
+      exit()
+  }
+  for(uint64_t i=0; i<DATASET_SIZE/HASH_BYTES;i++){
+      calcDatasetItem(seed, i, &(uint8_t*)out[i*HASH_BYTES]);
+  }
+ }
 void calcDatasetItem(uint8_t* seed, uint32_t itemNumber, uint8_t* out){
     blake2s(out, 32, itemNumber, 4, seed, 32);
 }
